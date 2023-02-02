ba45f0f7876f19629ee831ee04f3ff8d6a793381
Kaneryaa <kanerya7@gmail.com>
2023-02-02 15:20:31 +0000

    adding project

--- installation.sh
+++ installation.sh
@@ -1,10 +1,25 @@
 #!/bin/bash 
+echo "=================================="
+echo "                   installtion               "
 #apt-get update -y
 #apt-get install docker.io -y
 #curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 #sudo install minikube-linux-amd64 /usr/local/bin/minikube
 #sudo groupadd docker
 #sudo usermod -aG docker $USER
-minikube start --driver=docker  --force
-
-
+#minikube start --driver=docker  --force
+#docker ps 
+#minikube ssh
+#snap install kubectl --classic
+#kubectl get pods 
+#kubectl get pods --namespace=kube-system
+kubectl get namespace
+#kubectl create namespace node-todo-app
+echo "============================================"
+echo "       pod                          "
+#kubectl apply -f project_pod.yaml
+#kubectl get pods --namespace=node-todo-app
+#kubectl get nodes
+#minikube ssh
+#kubectl get pods --namespace=node-todo-app -o wide
+kubectl delete -f project_pod.yaml
