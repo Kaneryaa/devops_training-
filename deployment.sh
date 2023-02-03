5d82929b3ad027215fe16655f1e8adf52af57c18
Danish_Shaikh <kanerya7@gmail.com>
2023-02-03 12:23:55 +0530

    delete minikube

--- deployment.sh
+++ deployment.sh
@@ -6,4 +6,8 @@
 #kubectl expose deployment hello-minikube --type=NodePort --port=8080
 #minikube service hello-minikube --url
 #kubectl get svc
-kubectl delete svc hello-minikube
+#kubectl delete svc hello-minikube
+#kubectl get deploy
+#kubectl delete deploy hello-minikube
+#minikube stop
+minikube delete
