46ddedc3e770d3ac4adcbb11ffcfb464201f47a9
Danish_Shaikh <kanerya7@gmail.com>
2023-02-10 13:18:29 +0530

    kubeadm

---
+++ kubeadm kubernate/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"9ba93658d92db559bfdf512f5e35e9d267cb787156b86bee9ee6447940b618d3","vagrant_version":"2.3.1"}
