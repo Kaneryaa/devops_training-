be2084ab5355be60386819fd9ee17d47772d919c
Ubuntu <ubuntu@ip-172-31-61-111.ec2.internal>
2023-02-11 09:33:52 +0000

    install

---
+++ installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "=========================="
+echo " add configuration files "
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt update
+echo "================================"
+echo "installl Ansible"
+sudo apt install ansible
+
+
