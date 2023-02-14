87d3fe124fd527b7a7952f80fe50c46487f6deac
Ubuntu <ubuntu@ip-172-31-59-128.ec2.internal>
2023-02-14 05:16:45 +0000

    adding ansible.cgf

--- exercise/command.sh
+++ exercise/command.sh
@@ -2,4 +2,8 @@
 #sudo  vim /etc/ansible/ansible.cfg
 #ansible -i inventry -m ping websrvgrp
 #ansible -i inventry -m ping fullstack
-ansible -i inventry -m ping all
+#ansible -i inventry -m ping all
+#sudo touch /var/log/ansible.log
+#sudo chown ubuntu:ubuntu /var/log/ansible.log
+#cat /var/log/ansible.log
+ansible-playbook web-db.yaml -vv
