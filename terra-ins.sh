21fdaafce360436288824d0327ac6b14794be318
Ubuntu <ubuntu@ip-172-31-86-168.ec2.internal>
2023-03-04 15:29:00 +0000

    adding git installtion files

---
+++ terra-ins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+#wget -O- https://apt.releases.hashicorp.com/gpg | \
+#    gpg --dearmor | \
+#    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+#Verify the key's fingerprint.
+#gpg --no-default-keyring \
+#    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+#    --fingerprint
+#Add the official HashiCorp repository to your system. 
+#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+#    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+#    sudo tee /etc/apt/sources.list.d/hashicorp.list
+#Download the package information from HashiCorp.
+#sudo apt update
+#Install Terraform from the new repository.
+sudo apt-get install terraform
+
+
