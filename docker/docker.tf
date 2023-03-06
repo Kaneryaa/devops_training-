8c1a958129f43ad6b35f13b0212b83993bbe3eae
Ubuntu <ubuntu@ip-172-31-92-99.ec2.internal>
2023-03-06 12:53:09 +0000

    installing docker

---
+++ docker/docker.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = "Terraform-Nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
---
+++ docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo chown $USER /var/run/docker.sock
