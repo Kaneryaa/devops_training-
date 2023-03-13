76cabadfc2458da3a36e374133f292b49f7e4f6c
Danish_Shaikh <89991677+Kaneryaa@users.noreply.github.com>
2023-03-13 18:21:35 +0530

    Create aws-terraform.tf

---
+++ aws-terraform.tf
@@ -0,0 +1,23 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "my_aws_instance" {
+	ami = "ami-0557a15b87f6559cf"
+	instance_type = "t2.micro"
+	tags = {
+		name = "remote_lunch"
+	}
+}
