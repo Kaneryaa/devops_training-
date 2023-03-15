ca731cd27b703842a74fddce71f2c2c88b64ecf4
Danish_Shaikh <89991677+Kaneryaa@users.noreply.github.com>
2023-03-15 20:54:55 +0530

    Update meta.tf

--- exercise/meta.tf
+++ exercise/meta.tf
@@ -4,15 +4,19 @@ provider "aws" {
   #   secret_key = ""	
 }
 
+locals {
+	instance_name = toset(["front-end" , "back-end" , "tester"])
+}
+
 resource "aws_instance" "instance-create" {
-  count = 3
-  ami                    = "ami-03657b56516ab7912"
+  for_each = locals.instance_name
+  ami                    = "ami-005f9685cb30f234b"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1"
   #key_name               = "dove-key"
   #vpc_security_group_ids = ["sg-0780815f55104be8a"]
   tags = {
-    Name    = "Dove-Instance ${count.index}"
+    Name    = each.key
     Project = "Dove"
   }
 }
