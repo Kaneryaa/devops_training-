0f81628f379b9b43f55567ef24a4755dc66ae6e6
vagrant <vagrant@vagrant-docker>
2023-10-12 12:43:59 +0000

    os modu;e

--- os.py
+++ os.py
@@ -3,3 +3,16 @@ import os
 current_dir = os.getcwd()
 print(f"Cuurent directory:{current_dir}")
 
+files = os.listdir()
+print(f"files in current directory:{files}")
+
+file_path = 'examples.txt'
+if os.path.exists(file_path):
+    print(f"{file_path} exists")
+
+ 
+os.system('ls -l')
+
+pid = os.getpid()
+print(f"Process ID: {pid}")
+
